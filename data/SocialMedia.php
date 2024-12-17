92ace5b9f5a90f02ee3f1bc5804b960b043ffbb3
p4ddle <dewaputuadhitya@gmail.com>
2024-12-17 22:20:39 +0800

    final: function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -4,11 +4,22 @@ class SocialMedia{
 }
 
 //ketika kita membuat final class maka class tersebut tidak bisa di warisi 
-final class Facebook extends SocialMedia{
+class Facebook extends SocialMedia{
 
     public function Owner():void {
         echo "pemilik Facebook adalah $this->name" .PHP_EOL;
     }
+
+    final public function login(string $username, string $password):bool{
+        return true;
+    }
+}
+
+class FakeFacebook extends Facebook {
+    // public function login(string $username, string $password):bool{
+    //     return true;
+    // }
+    // kita tidak bisa meng override fnal function di child class 
 }
 //erorr
 // class FakeFacebook extends Facebook{ 
