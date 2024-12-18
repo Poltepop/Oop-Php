ddfaef20d10cc0b98a2c5ea2d2001dc766bcd3cb
p4ddle <dewaputuadhitya@gmail.com>
2024-12-18 13:45:12 +0800

    anonymous: construct

--- data/AnonymousClass.php
+++ data/AnonymousClass.php
@@ -1,11 +1,22 @@
 <?php
 interface HelloWorld {
     public function sayhello():void;
+
 }
 
-$helloWorld = new class implements helloWorld {
+$helloWorld = new class("Anonymous class") implements helloWorld {
+
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+
+
     function sayhello():void {
-        echo "Hello Anonymous Function class";
+        echo "Hello $this->name";
     }
 };
 
